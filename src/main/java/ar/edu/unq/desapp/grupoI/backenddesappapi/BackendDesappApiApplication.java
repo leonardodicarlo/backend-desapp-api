4e9c55584ee54f9c695552ad859edcf27ab39365
nahuelmpereyra <nahuelmpereyra@gmail.com>
2021-11-27 15:34:35 -0300

    Cache con redis local

--- src/main/java/ar/edu/unq/desapp/grupoI/backenddesappapi/BackendDesappApiApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupoI/backenddesappapi/BackendDesappApiApplication.java
@@ -3,17 +3,18 @@ package ar.edu.unq.desapp.grupoI.backenddesappapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableScheduling
 @ConditionalOnProperty(
 		value = "app.scheduling.enable", havingValue = "true", matchIfMissing = true
 )
+@EnableCaching
 @Configuration
 public class BackendDesappApiApplication {
 
